ab2bfcc4f4cc9589759cedceaa6e1ab41e945d00
Your Name <you@example.com>
2024-01-18 20:11:12 +0530

    hello

---
+++ index.js
@@ -0,0 +1,45 @@
+         // objects
+
+        //  let students=
+        //  {
+
+        //     name:'vishal',
+        //     age:'21',
+        //     ispass:'true'
+        //  }
+ 
+        //  students.name
+        //  console.log(students)
+
+
+let obj=
+{
+    user:'modi',
+    lastname:'ji',
+    sum:function()
+    {
+        console.log(this.user+this.lastname)
+    }
+
+}
+
+obj.sum();
+
+
+
+        //function
+        
+       function average()
+       {
+        return (a+b)/2
+       }
+        let a=2;
+        let b=3;
+        let c=4;
+
+        
+        console.log("average of a and b is" ,(a+b)/2)
+        console.log("average of a and c is" ,(a+b/2)/2)
+
+        console.log("average of b and c is", (a+b/2)/2)
+
